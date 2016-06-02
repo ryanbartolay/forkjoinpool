482176b8b8fa56500f5791ccd9932493122ba357
Ryan Kristoffer Bartolay <bartolay.ryan@gmail.com>
2016-06-02 18:12:52 +0800

    concurrency on fork join pool

--- forkjoinpool/src/main/java/com/ryan/concurrency/forkjoinpool/App.java
+++ forkjoinpool/src/main/java/com/ryan/concurrency/forkjoinpool/App.java
@@ -1,13 +1,42 @@
 package com.ryan.concurrency.forkjoinpool;
 
-/**
- * Hello world!
- *
- */
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+	public static void main( String[] args ) {    	
+		/*	ForkJoinPool forkJoinPool = new ForkJoinPool(4);
+    	MyRecursiveAction myRecursiveAction = new MyRecursiveAction(24);
+
+    	forkJoinPool.invoke(myRecursiveAction);*/		
+
+		try {
+			Properties properties = new Properties();
+			properties.load(new FileInputStream(new File("jndi.properties")));
+		
+	
+			Context initCtx = new InitialContext(properties);
+			Context envCtx = (Context) initCtx.lookup("java:jboss/datasources/ExampleDS");
+
+
+			System.out.println(envCtx);
+		} catch (NamingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
---
+++ forkjoinpool/src/main/java/com/ryan/concurrency/forkjoinpool/Main.java
@@ -0,0 +1,9 @@
+package com.ryan.concurrency.forkjoinpool;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Ryan");
+	}
+
+}
